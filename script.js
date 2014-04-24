0ccf06a3f288609265f20fb8379273eefb6816d6
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-04-23 19:30:47 -0700

    make chat feature

--- script.js
+++ script.js
@@ -167,7 +167,10 @@ function TickTime($scope, $resource, $timeout){
 		}
 		while ($scope.board[compSq].resident !==0);
 		$scope.board[compSq].resident = 5;
-		$scope.board[compSq].xoxo = "0";
+		$scope.board[compSq].xoxo = "O";
+		var compMessageArray = ["I don't know what I'm doing", "This game does not compute", "Errrrrr", "Ummmmmmm", "Wow, you're really good at this", "I suck", "Fucking tic tac toe", "You can't blame me, I don't have a brain", "take it easy, won't you", "i'm hungry, i haven't eaten.. ever", "would you consider me 'intelligent?'", "do you want to build a snowman?", "i'm chompy and I like warm hugs", "do you think i'm sexy?", "ur mean, i don't like you", "poop poop poop", "waaaah", "soo.. i was reading a really interesting article but I don't remember what it's about", "why aren't there any toes in tic tac toe", "i don't have any toes", "lalaallal, the Os look like 0s"];
+		$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+		gotoBottom();
 		winCheck();
 		
 	}
@@ -186,6 +189,9 @@ function TickTime($scope, $resource, $timeout){
 							compSq = winningCombos[i][k];
 							turnwent = 1;
 							console.log("winning")
+								var compMessageArray = ["I win"];
+								$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+								gotoBottom();
 						};
 					};
 				};
@@ -202,6 +208,9 @@ function TickTime($scope, $resource, $timeout){
 							if($scope.board[winningCombos[i][k]].resident ==0){
 								compSq = winningCombos[i][k];
 								turnwent = 1;
+									var compMessageArray = ["Oh no you diint", "I don't think so loser", "very sneaky, but no.."];
+									$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+									gotoBottom();
 								console.log("blocking")
 							};
 						};
@@ -213,6 +222,9 @@ function TickTime($scope, $resource, $timeout){
 		if (turnwent == 0){
 			do{
 				compSq = Math.floor($scope.board.length*Math.random());
+				var compMessageArray = ["I don't know what I'm doing", "This game does not compute", "Errrrrr", "Ummmmmmm", "Wow, you're really good at this", "I suck", "Fucking tic tac toe"];
+				$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+				gotoBottom();
 			}
 			while ($scope.board[compSq].resident !==0);
 		};
@@ -234,7 +246,10 @@ function TickTime($scope, $resource, $timeout){
 						if($scope.board[winningCombos[i][k]].resident ==0){
 							compSq = winningCombos[i][k];
 							turnwent = 1;
-							console.log("winning")
+								var compMessageArray = ["winning (in charlie sheen voice)", "mmmmhmmm", "very nice, I like", "I winnnnn"];
+							$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+							gotoBottom();
+							console.log("winning");
 						};
 					};
 				};
@@ -251,6 +266,9 @@ function TickTime($scope, $resource, $timeout){
 							if($scope.board[winningCombos[i][k]].resident ==0){
 								compSq = winningCombos[i][k];
 								turnwent = 1;
+									var compMessageArray = ["Erm, no", "nice try stupid", "wow, you're almost there", "i like that you try"];
+									$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+									gotoBottom();
 								console.log("blocking")
 							};
 						};
@@ -283,6 +301,9 @@ function TickTime($scope, $resource, $timeout){
 			if(commonindex.length>0){
 			compSq = commonindex[0];
 			turnwent = 1;
+				var compMessageArray = ["I'm forking, you're doomed", "stick a fork in you, you're done!", "sorry, i have to do this..."];
+				$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+				gotoBottom();
 			console.log("I'm forking");
 			};
 		};
@@ -331,6 +352,9 @@ function TickTime($scope, $resource, $timeout){
 						if(countermat == 0){
 							compSq = ocombos[v][0];
 							turnwent = 1;
+								var compMessageArray = ["I'm on to your forky ways", "I know what you were about to do even if you don't", "You're awesome for trying that fork, but no!!!"];
+							$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+							gotoBottom();
 							console.log("I'm blocking" + compSq);
 						};			
 					};
@@ -344,6 +368,9 @@ function TickTime($scope, $resource, $timeout){
 						if(countermat == 0){
 							compSq = ocombos[v][1];
 							turnwent = 1;
+								var compMessageArray = ["I'm on to your forky ways", "I know what you were about to do even if you don't", "You're awesome for trying that fork, but no!!!"];
+								$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+								gotoBottom();
 							console.log("I'm blocking" + compSq);
 						};			
 					};
@@ -357,6 +384,9 @@ function TickTime($scope, $resource, $timeout){
 						if(countermat == 0){
 							compSq = ocombos[v][2];
 							turnwent = 1;
+								var compMessageArray = ["I'm on to your forky ways", "I know what you were about to do even if you don't", "You're awesome for trying that fork, but no!!!"];
+								$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+								gotoBottom();
 							console.log("I'm blocking" + compSq);
 						};			
 					};
@@ -369,48 +399,72 @@ function TickTime($scope, $resource, $timeout){
 				console.log("0open1");
 					compSq = 6;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}
 			else if($scope.board[0].resident ==5 && $scope.board[2].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("0open2");
 					compSq = 2;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}
 			else if($scope.board[2].resident ==5 && $scope.board[8].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("2open1");
 					compSq = 8;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}
 			else if($scope.board[2].resident ==5 && $scope.board[0].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("2open2");
 					compSq = 0;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}
 			else if($scope.board[6].resident ==5 && $scope.board[0].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("6open1");
 					compSq = 0;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}
 			else if($scope.board[6].resident ==5 && $scope.board[8].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("6open2");
 					compSq = 8;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}
 			else if($scope.board[8].resident ==5  && $scope.board[2].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("8open1");
 					compSq = 2;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}	
 			else if($scope.board[8].resident ==5 && $scope.board[6].resident ==0 && $scope.board[4].resident ==0 && $scope.board[0].resident+$scope.board[2].resident+$scope.board[6].resident+$scope.board[8].resident==8){
 				console.log("8open2");
 					compSq = 6;
 					turnwent = 1;
+					var compMessageArray = ["you're doomed", "You're going to lose now, should have picked center", "you suck, second should always pick center", "hehehehehheheh", "lolz i don't even have a brain"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to myself");
 			}	
 		}
@@ -420,6 +474,9 @@ function TickTime($scope, $resource, $timeout){
 			if($scope.board[4].resident ==0){
 					compSq = 4;
 					turnwent = 1;
+					var compMessageArray = ["it seems nice and safe here", "i'm in the middle of everything", "i'm the center of my own universe", "don't box me in in in in in in ..."];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("center");
 			};
 		};
@@ -430,6 +487,9 @@ function TickTime($scope, $resource, $timeout){
 				if($scope.board[8].resident ==0){
 					compSq = 8;
 					turnwent = 1;
+					var compMessageArray = ["I read on a website I should do this", "Yeah, I guess I might as well go here"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to opp");
 				};
 			}
@@ -437,6 +497,9 @@ function TickTime($scope, $resource, $timeout){
 				if($scope.board[6].resident ==0){
 					compSq = 6;
 					turnwent = 1;
+					var compMessageArray = ["I read on a website I should do this", "Yeah, I guess I might as well go here"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to opp");
 				};
 			}
@@ -444,6 +507,9 @@ function TickTime($scope, $resource, $timeout){
 				if($scope.board[2].resident ==0){
 					compSq = 2;
 					turnwent = 1;
+					var compMessageArray = ["I read on a website I should do this", "Yeah, I guess I might as well go here"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to opp");
 				};
 			}
@@ -451,6 +517,9 @@ function TickTime($scope, $resource, $timeout){
 				if($scope.board[0].resident ==0){
 					compSq = 0;
 					turnwent = 1;
+					var compMessageArray = ["I read on a website I should do this", "Yeah, I guess I might as well go here"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("opp corner to opp");
 				};
 			};		
@@ -461,22 +530,34 @@ function TickTime($scope, $resource, $timeout){
 			if($scope.board[0].resident ==0){
 					compSq = 0;
 					turnwent = 1;
+					var compMessageArray = ["I can see the world from my corner", "lalalalalal", "poop"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("empty corner");
 			}
 			else if($scope.board[2].resident ==0){
 					compSq = 2;
 					turnwent = 1;
 					console.log("empty corner");
+					var compMessageArray = ["I can see the world from my corner", "lalalalalal", "poop"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 			}
 			else if($scope.board[6].resident ==0){
 					compSq = 6;
 					turnwent = 1;
 					console.log("empty corner");
+					var compMessageArray = ["I can see the world from my corner", "lalalalalal", "poop"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 			}
 			else if($scope.board[8].resident ==0){
 					compSq = 8;
 					turnwent = 1;
 					console.log("empty corner");
+					var compMessageArray = ["I can see the world from my corner", "lalalalalal", "poop"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 			};
 		};
 		//choose an empty side
@@ -485,20 +566,32 @@ function TickTime($scope, $resource, $timeout){
 					compSq = 1;
 					turnwent = 1;
 					console.log("empty side");
+					var compMessageArray = ["I'm all alone over here", "I'm scared and alone"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 			}
 			else if($scope.board[0].resident + $scope.board[3].resident + $scope.board[6].resident ==0){
 					compSq = 3;
 					turnwent = 1;
+					var compMessageArray = ["I'm all alone over here", "I'm scared and alone"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("empty side");
 			}
 			else if($scope.board[2].resident + $scope.board[5].resident + $scope.board[8].resident ==0){
 					compSq = 5;
 					turnwent = 1;
+					var compMessageArray = ["I'm all alone over here", "I'm scared and alone"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("empty side");
 			}
 			else if($scope.board[6].resident + $scope.board[7].resident + $scope.board[8].resident ==0){
 					compSq = 7;
 					turnwent = 1;
+					var compMessageArray = ["I'm all alone over here", "I'm scared and alone"];
+					$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+					gotoBottom();
 					console.log("empty side");
 			};
 		};
@@ -506,11 +599,15 @@ function TickTime($scope, $resource, $timeout){
 		if (turnwent == 0){
 			do{
 				compSq = Math.floor($scope.board.length*Math.random());
+				var compMessageArray = ["Ermmmm", "whatever, just go with it", "i go with the flow man"];
+				$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
+				gotoBottom();
 			}
 			while ($scope.board[compSq].resident !==0);
 		};
 		$scope.board[compSq].resident = 5;
 		$scope.board[compSq].xoxo = "O";
+
 		winCheck();
 	}
 	$scope.winResult = null;
@@ -540,6 +637,8 @@ function TickTime($scope, $resource, $timeout){
 				$scope.xwins +=1;
 				$scope.lastWinner = 3;
 				$timeout(function(){resetBoard()}, 2000);
+				var compMessageArray = ["congrats loser", "i'm not jelly", "congrats on your 'win'", "yayyyy you're great", "yoo so smart :) <3", "you're so good at things, plz be my friend", "i hate you"];
+							$scope.chats.push({yourName:"Compy", yourMessage:compMessageArray[Math.floor(Math.random()*compMessageArray.length)]});
 			};
 			if(total == 15){
 				var index = winningCombos[i][0];
@@ -565,6 +664,7 @@ function TickTime($scope, $resource, $timeout){
 			tieTot = theb[tiepos[0]].resident + theb[tiepos[1]].resident + theb[tiepos[2]].resident;
 			if((tieTot %3 != 0) && (tieTot %5 != 0)){
 				tieCheck +=1;
+				console.log(tieCheck);
 			};
 			if(tieCheck == 8){
 				gameOver = 1;
@@ -614,7 +714,16 @@ function TickTime($scope, $resource, $timeout){
 			$scope.activate = "notactive";
 		};
 	}
-	
+	$scope.username = "Melanie";
+	$scope.message = "hi";
+	$scope.chats=[{yourName:$scope.username, yourMessage:$scope.message}];
+	$scope.addChat = function(){
+		console.log('executed');
+		$scope.chats.push({yourName:$scope.username, yourMessage:$scope.message});
+	};
+	function gotoBottom(){
+		document.getElementById("chatMain").scrollTop = document.getElementById("chatMain").scrollHeight;
+	};
 
 	
 };
